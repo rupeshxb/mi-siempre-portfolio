f8803d1c8c4689324f847c7284be296b8f40c90f
rupeshxb <rupesh.bhatta1234@gmail.com>
2022-08-10 12:09:52 +0545

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,38 +60,5 @@ function scrollNavbar() {
 
 // ------- Dynamic, shrinking navbar ----------------- //
 
-/**
- * @author san randry
- * @license MIT license
- * this plugin disable all inspect element feature on a web browser
- */
-// disable right click
-document.oncontextmenu = function (e) {
-  e.preventDefault();
-  return false;
-}
-// disable somme additional key
-document.onkeydown = function (e) {
-  e.preventDefault()
-  // disable f12
-  if (e.keycode == 123) {
-      return false;
-  }
-  // disable ctrl+shift+I
-  if (e.ctrlKey && e.shiftKey && e.keycode == 'i'.charCodeAt(0)){
-      return false;
-  }
-  // disable ctrl+shift+C
-  if (e.ctrlKey && e.shiftKey && e.keycode == 'C'.charCodeAt(0)){
-      return false;
-  }
-  // disable ctrl+shift+j
-  if (e.ctrlKey && e.shiftKey && e.keycode == 'J'.charCodeAt(0)){
-      return false;
-  }
-  // disable ctrl+shift+U
-  if (e.ctrlKey && e.keycode == 'U'.charCodeAt(0)) {
-      return false;
-  }
-}
+
 
