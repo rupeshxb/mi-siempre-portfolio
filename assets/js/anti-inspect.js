2879ef45af5ac506cb17bd864e1642586ae648ff
rupeshxb <rupesh.bhatta1234@gmail.com>
2022-08-10 20:23:28 +0545

    done

--- assets/js/anti-inspect.js
+++ assets/js/anti-inspect.js
@@ -6,28 +6,34 @@
 // disable right click
 document.oncontextmenu = function (e) {
   e.preventDefault();
+  alert("Action Denied");
   return false;
 }
 // disable somme additional key
 document.onkeydown = function (e) {
   // disable f12
   if (e.keycode == 123) {
+    e.preventDefault();
       return false;
   }
   // disable ctrl+shift+I
   if (e.ctrlKey && e.shiftKey && e.keycode == 'i'.charCodeAt(0)){
-      return false;
+    alert("Hey you! Stop what you\'re doing");
+    return false;
   }
   // disable ctrl+shift+C
   if (e.ctrlKey && e.shiftKey && e.keycode == 'C'.charCodeAt(0)){
+    
       return false;
   }
   // disable ctrl+shift+j
   if (e.ctrlKey && e.shiftKey && e.keycode == 'J'.charCodeAt(0)){
+    
       return false;
   }
   // disable ctrl+shift+U
   if (e.ctrlKey && e.keycode == 'U'.charCodeAt(0)) {
+    
       return false;
   }
 }
