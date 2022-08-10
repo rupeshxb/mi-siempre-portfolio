1d0843066e347be243df613adf5ba61f9b3d7eb6
rupeshxb <rupesh.bhatta1234@gmail.com>
2022-08-10 10:58:41 +0545

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,19 +77,19 @@ document.onkeydown = function (e) {
       return false;
   }
   // disable ctrl+shift+I
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+  if (e.ctrlKey && e.shiftKey && e.keycode == 'I'.charCodeAt(0)) {
       return false;
   }
   // disable ctrl+shift+C
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+  if (e.ctrlKey && e.shiftKey && e.keycode == 'C'.charCodeAt(0)) {
       return false;
   }
   // disable ctrl+shift+j
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+  if (e.ctrlKey && e.shiftKey && e.keycode == 'J'.charCodeAt(0)) {
       return false;
   }
   // disable ctrl+shift+U
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+  if (e.ctrlKey && e.keycode == 'U'.charCodeAt(0)) {
       return false;
   }
 }
