b8ce7c54131aa77fb5454ba41ca5323db2d67e60
rupeshxb <rupesh.bhatta1234@gmail.com>
2022-08-16 17:20:41 +0545

    done

--- assets/js/anti-inspect.js
+++ assets/js/anti-inspect.js
@@ -1,42 +1,50 @@
-/**
- * @author san randry
- * @license MIT license
+/*
  * this plugin disable all inspect element feature on a web browser
  */
-// disable right click
 
-// document.oncontextmenu = function (e) {
-//   e.preventDefault();
-//   alert("Action Denied");
-//   return false;
-// }
+// disable right click
+document.oncontextmenu = function (e) {
+  e.preventDefault();
+  alert("Action Denied");
+  return false;
+}
 
 // disable somme additional key
 document.onkeydown = function (e) {
   // disable f12
   if (e.keycode == 123) {
     e.preventDefault();
+    alert("Action not allowed")
       return false;
   }
+  var countPressZ=0;
+  while(e.key='z'){
+    countPressZ++;
+  }
+  console.log(countPressZ);
   // disable ctrl+shift+I
-  // && e.keycode == 'i'.charCodeAt(0)
-  if (e.ctrlKey && e.shiftKey){
-    console.log("Hey you! Stop what you\'re doing");
-    return false;
+  if (e.ctrlKey && e.shiftKey && (e.key=='I' || e.key=='i')){
+      e.preventDefault();
+      alert("Denied to inspect.");
+      return false;
   }
+  
   // disable ctrl+shift+C
-  if (e.ctrlKey && e.shiftKey && e.keycode == 'C'.charCodeAt(0)){
-    
-      return false;
-  }
+  if (e.ctrlKey && e.shiftKey && (e.key=='c' || e.key=='C')){
+    e.preventDefault();
+    alert("Action not allowed");
+    return false;
+}
   // disable ctrl+shift+j
-  if (e.ctrlKey && e.shiftKey && e.keycode == 'J'.charCodeAt(0)){
-    
-      return false;
-  }
+  if (e.ctrlKey && e.shiftKey && (e.key=='J' || e.key=='j')){
+    e.preventDefault();
+    alert("Action not allowed");
+    return false;
+}
   // disable ctrl+shift+U
-  if (e.ctrlKey && e.keycode == 'U'.charCodeAt(0)) {
-    
-      return false;
+  if (e.ctrlKey && e.shiftKey && (e.key=='u' || e.key=='U')){
+    e.preventDefault();
+    alert("Action not allowed");
+    return false;
   }
 }
